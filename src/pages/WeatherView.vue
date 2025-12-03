fcfdd458990b7e8337d702fb0bd7af0196dea59e
Maxim Donnikov <hf35@ya.ru>
2025-12-03 13:07:20 +0500

    chore

--- src/pages/WeatherView.vue
+++ src/pages/WeatherView.vue
@@ -3,7 +3,7 @@ import { AdjustmentsHorizontalIcon } from "@heroicons/vue/24/outline";
 import { ref, onMounted, watch, computed } from "vue"
 import { useWidgetStore } from "../stores/useWidgetStore"
 import { getWeatherForCoords } from "../services/weatherService"
-import { getDefaultLocation } from "../services/getDefaultLocation";
+import { getDefaultLocation } from "../utils/getDefaultLocation";
 import { searchCityByCoords } from "../services/geoService"
 import WeatherViewItem from "../components/WeatherViewItem.vue"
 
@@ -93,20 +93,6 @@ onMounted(() => {
     <div v-if="errorDefLocation" class=" text-center">
       {{ errorDefLocation }}
     </div>
-    <!-- <template v-if="enrichedCities">
-      <template v-for="city in enrichedCities">
-        <div v-if="city.weather" class="border-solid border-gray-100 mb-2 px-2 rounded-md">
-          <h3 class="relative text-lg font-bold "> {{ city.name }}
-            <SunIcon v-if="city.weather.is_day" class="h-6 w-6 text-gray-500 absolute top-0 right-2 " />
-            <MoonIcon v-else class="h-6 w-6 text-gray-500 absolute top-0 right-2 " />
-          </h3>
-          <p>Temp. {{ city.weather.temperature_2m.toFixed(0) }} °C</p>
-          <p>Cloud cover {{ city.weather.cloud_cover }} %</p>
-          <p>Rel. humidity {{ city.weather.relative_humidity_2m }} %</p>
-
-        </div>
-      </template>
-    </template> -->
     <WeatherViewItem :enrichedCities="enrichedCities" />
   </div>
 </template>
